1a1462886769d4bca191868f44a3ee4ad61dac99
Ayana Bando <ayanaflax@yahoo.com>
2018-11-21 15:09:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ function scuberGreetingForFeet(feet){
   return result
 }
 function ternaryCheckCity(city){
-   return (city ? "Ok, sounds good." : "No go.");
+   return (city === "NYC" ? "Ok, sounds good." : "No go.");
   }
