cd0875be1e0735f11dbd63d41accb3faa291038b
Ayana Bando <ayanaflax@yahoo.com>
2018-11-21 15:13:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function ternaryCheckCity(city){
   
   function switchOnCharmFromTip(tip){
     switch (tip){
-      case 'generous':
+      case tip ==='generous':
         console.log("Thank you so much." );
         break;
     }
